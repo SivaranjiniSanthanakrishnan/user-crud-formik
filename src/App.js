e0b93de354c52cae426a2862f20609cb2d207859
SivaranjiniSanthanakrishnan <43466009+SivaranjiniSanthanakrishnan@users.noreply.github.com>
2022-09-25 18:38:09 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -20,12 +20,19 @@ import FormLabel from "@mui/material/FormLabel";
 import MenuItem from "@mui/material/MenuItem";
 import InputLabel from "@mui/material/InputLabel";
 import Select, { SelectChangeEvent } from "@mui/material/Select";
+import { Formik } from "formik";
 
 function App() {
   const [userData, setUserData] = useState([]);
-
+  const [formData, setFormData] = useState({
+    id: "",
+    name: "",
+    age: "",
+    email: "",
+    gender: "",
+    courses: "",
+  });
   useEffect(() => {
-    // API Call
     async function getData() {
       const response = await axios.get(
         "https://61fcdb8ff62e220017ce41c1.mockapi.io/users"
@@ -34,59 +41,168 @@ function App() {
     }
     getData();
   }, []);
+  const onPopulateData = (id) => {
+    const selectedData = userData.filter((row) => row.id === id)[0];
+    setFormData({ ...selectedData });
+  };
+  const validateForm = (formDataToValidate) => {
+    var error = {};
+    if (formDataToValidate.name === "") error.name = "Name is Required";
+    if (formDataToValidate.email === "") error.email = "Email is Required";
+    if (formDataToValidate.age === "") error.age = "Age is Required";
+    if (formDataToValidate.gender === "") error.gender = "Gender is Required";
+    if (formDataToValidate.courses === "")
+      error.courses = "Courses is Required";
+    return error;
+  };
+  const handleSubmit = async (formSubmittedData, { resetForm }) => {
+    if (formData.id) {
+      // Update
+      const response = await axios.put(
+        `https://61fcdb8ff62e220017ce41c1.mockapi.io/users/${formData.id}`,
+        { ...formSubmittedData }
+      );
+      let user = [...userData];
+      let index = userData.findIndex((row) => row.id === formData.id);
+      user[index] = response.data;
+      setUserData(user);
+      resetForm();
+    } else {
+      // Create
+      const response = await axios.post(
+        "https://61fcdb8ff62e220017ce41c1.mockapi.io/users",
+        { ...formSubmittedData }
+      );
+      setUserData([...userData, response.data]);
+      resetForm();
+    }
+  };
   return (
     <div style={{ padding: "10px" }}>
       <h3>User Form</h3>
-      <Box
-        component="form"
-        sx={{
-          "& > :not(style)": { m: 1, width: "25ch" },
-        }}
-        noValidate
-        autoComplete="off"
+      <Formik
+        initialValues={formData}
+        validate={validateForm}
+        onSubmit={handleSubmit}
+        enableReinitialize={true}
       >
-        <TextField id="name" label="Name" variant="outlined" /> <br />
-        <TextField type="number" id="age" label="Age" variant="outlined" />{" "}
-        <br />
-        <TextField id="email" label="Email" variant="outlined" /> <br />
-        {/* Gender */}
-        <FormControl>
-          <FormLabel id="demo-radio-buttons-group-label">Gender</FormLabel>
-          <RadioGroup
-            aria-labelledby="demo-radio-buttons-group-label"
-            defaultValue="female"
-            name="gender"
-            id="gender"
+        {({
+          values,
+          errors,
+          touched,
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          isSubmitting,
+          /* and other goodies */
+        }) => (
+          <Box
+            component="form"
+            sx={{
+              "& > :not(style)": { m: 1, width: "25ch" },
+            }}
+            noValidate
+            autoComplete="off"
+            onSubmit={handleSubmit}
           >
-            <FormControlLabel
-              value="female"
-              control={<Radio />}
-              label="Female"
+            <TextField
+              id="name"
+              name="name"
+              label="Name"
+              variant="outlined"
+              value={values.name}
+              onChange={handleChange}
+              onBlur={handleBlur}
             />
-            <FormControlLabel value="male" control={<Radio />} label="Male" />
-            <FormControlLabel value="other" control={<Radio />} label="Other" />
-          </RadioGroup>
-        </FormControl>
-        <br />
-        <FormControl fullWidth>
-          <InputLabel id="demo-simple-select-label">Courses</InputLabel>
-          <Select
-            labelId="demo-simple-select-label"
-            id="courses"
-            name="courses"
-            label="Courses"
-          >
-            <MenuItem value={"React JS"}>React JS</MenuItem>
-            <MenuItem value={"Node JS"}>Node JS</MenuItem>
-            <MenuItem value={"MySQL"}>MySQL</MenuItem>
-          </Select>
-        </FormControl>{" "}
-        <br /> <br />
-        <Button variant="contained" type="submit">
-          Save
-        </Button>
-        <Button variant="contained">Reset</Button>
-      </Box>
+            <br />
+            <span style={{ color: "red" }}>{touched.name && errors.name}</span>
+            <br />
+            <TextField
+              type="number"
+              id="age"
+              label="Age"
+              variant="outlined"
+              value={values.age}
+              onChange={handleChange}
+              onBlur={handleBlur}
+            />
+            <br />
+            <span style={{ color: "red" }}>{touched.age && errors.age}</span>
+            <br />
+            <TextField
+              id="email"
+              label="Email"
+              variant="outlined"
+              value={values.email}
+              onChange={handleChange}
+              onBlur={handleBlur}
+            />
+            <br />
+            <span style={{ color: "red" }}>
+              {touched.email && errors.email}
+            </span>
+            <br />
+            {/* Gender */}
+            <FormControl>
+              <FormLabel id="demo-radio-buttons-group-label">Gender</FormLabel>
+              <RadioGroup
+                aria-labelledby="demo-radio-buttons-group-label"
+                name="gender"
+                id="gender"
+                value={values.gender}
+                onChange={handleChange}
+                onBlur={handleBlur}
+              >
+                <FormControlLabel
+                  value="female"
+                  control={<Radio />}
+                  label="Female"
+                />
+                <FormControlLabel
+                  value="male"
+                  control={<Radio />}
+                  label="Male"
+                />
+                <FormControlLabel
+                  value="other"
+                  control={<Radio />}
+                  label="Other"
+                />
+              </RadioGroup>
+            </FormControl>
+            <br />
+            <span style={{ color: "red" }}>
+              {touched.gender && errors.gender}
+            </span>
+            <br />
+            <FormControl fullWidth>
+              <InputLabel id="label-courses">Courses</InputLabel>
+              <Select
+                labelId="label-courses"
+                id="courses"
+                name="courses"
+                label="Courses"
+                value={values.courses}
+                onChange={handleChange}
+                onBlur={handleBlur}
+              >
+                <MenuItem value={"React JS"}>React JS</MenuItem>
+                <MenuItem value={"Node JS"}>Node JS</MenuItem>
+                <MenuItem value={"MySQL"}>MySQL</MenuItem>
+              </Select>
+            </FormControl>
+            <br />
+            <span style={{ color: "red" }}>
+              {touched.courses && errors.courses}
+            </span>
+            <br />
+            <Button variant="contained" type="submit">
+              Save
+            </Button>
+            <Button variant="contained">Reset</Button>
+          </Box>
+        )}
+      </Formik>
 
       <h3>User Data</h3>
       <TableContainer component={Paper}>
@@ -114,7 +230,9 @@ function App() {
                 <TableCell align="right">{row.gender}</TableCell>
                 <TableCell align="right">{row.courses}</TableCell>
                 <TableCell>
-                  <Button variant="text">Edit</Button>
+                  <Button variant="text" onClick={() => onPopulateData(row.id)}>
+                    Edit
+                  </Button>
                   <Button variant="text">Delete</Button>
                 </TableCell>
               </TableRow>
